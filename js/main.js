4c536825a19c0a9610d675dbece1a0d0004a8cb4
ZachyDev <mosetizachary001@gmail.com>
2020-06-14 00:51:20 +0300

    function scope demo in javascript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-// function outputUser
+// // function outputUser
 
+// const outputDetails = (name) => {
+//     let message = 'hello';
+// }
+// outputDetails('Zachy');
+// console.log(message) //reference error - its out of scope
+
+// working 
 const outputUser = (name) => {
-    let message = 'hello';
+    let msg = `Hello ${ name }`;
+    // inner function
+    let userInfo = (msg) => {
+        console.log(msg);
+    }
+    userInfo(msg);
 }
 outputUser('Zachy');
-console.log(message) //reference error - its out of scope
